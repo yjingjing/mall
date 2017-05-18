31c56140ff29ee69d115cfb6f3266555835f3227
yjingjing <2029896522@qq.com>
2017-05-18 22:34:37 +0800

    first commit

---
+++ routes/cart.js
@@ -0,0 +1,70 @@
+module.exports=function(app){
+	app.get('/cart',function(req,res){
+		var Cart=global.dbHelper.getModel('cart');
+		if(!req.session.user){
+			req.session.error='用户已过期，请重新登陆：';
+			res.redirect('/login');
+		}else{
+			Cart.find({'uId':req.session.user._id,'cStatus':false},function(error,docs){
+				res.render('cart',{carts:docs});
+			});
+		}
+	});
+	app.get('/addToCart/:id',function(req,res){
+		if(!req.session.user){
+			req.session.error='用户已过期，请重新登陆：';
+			res.redirect('/login');
+		}else{
+			var Commodity=global.dbHelper.getModel('commodity'),
+				Cart=global.dbHelper.getModel('cart');
+			Cart.findOne({'uId':req.session.user._id,"cId":req.params.id},function(error,doc){
+				if(doc){
+					Cart.update({"uId":req.session.user._id,"cId":req.params.id},{$set:{cQuantity:co.cQuantity+1}},function(error,doc){
+						if(doc>0){
+							res.redirect('/home');
+						}
+					});
+				}else{
+					Commodity.findOne({"_id":req.params.id},function(error,doc){
+						if(doc){
+							Cart.create({
+								uId:req.session.user._id,
+								cId:req.params.id,
+								cName:doc.name,
+								cPrice:doc.price,
+								cImgSrc:doc.imgSrc,
+								cQuantity:1
+							},function(error,doc){
+								if(doc){
+									res.redirect('/home');
+								}
+							});
+						}else{
+							
+						}
+					});
+				}
+			});
+		}
+	});
+	//删除购物车商品
+	app.get("/delFromCart/:id",function(req,res){
+		var Cart=global.dbHelper.getModel('cart');
+		Cart.remove({"_id":req.params.id},function(error,doc){
+			//成功返回1，失败返回0
+			if(doc>0){
+				res.redirect('/cart');
+			}
+		});
+	});
+	//购物车结算
+	app.post("/cart/clearing",function(req,res){
+		var Cart=global.dbHelper.getModel('cart');
+		Cart.update({"_id":req.body.cid},{$set:{cQuantity:req.body.cnum,cStatus:true}},function(error,doc){
+			//更新成功返回1，失败返回0
+			if(doc>0){
+				res.send(200);
+			}
+		});
+	});
+}
---
+++ routes/login.js
@@ -0,0 +1,26 @@
+module.exports=function(app){
+	app.get('/login',function(req,res){
+		res.render('login');
+	});
+	app.post('/login',function(req,res){
+		var User=global.dbHelper.getModel('user');
+		var uname=req.body.uname,upwd=req.body.upwd;
+		User.findOne({name:uname},function(error,doc){
+			if(error){
+				res.send(500);
+				console.log(error);
+			}else if(!doc){
+				req.session.error='用户名不存在！';
+				res.send(404);
+			}else{
+				if(upwd!=doc.password){
+					req.session.error='密码错误！';
+					res.send(404);
+				}else{
+					req.session.user=doc;
+					res.send(200);
+				}
+			}
+		});
+	});
+}
